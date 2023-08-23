602d8e8193859dc5b09192fb7ff60d0b36245e76
onurkaymak <onurkaymak@outlook.com>
2023-08-23 10:50:02 -0700

    Add Player func for Test#1, passing.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//Business Logic
+
+function Player(name, turnTotal, total) {
+    this.player = name;
+    this.turnTotal = turnTotal;
+    this.total = total;
+    this.turn = false;
+}
+
+// const player1 = new Player(1, 0, 0);
+// const player2 = new Player(2, 0, 0);
+
+// console.log(player1);
+// console.log(player2);
